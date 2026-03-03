bb0e95cf496a0921f2fde5aecc1be3ed2f95330b
linyuan.yang <linyuan.yang@centurygame.com>
2026-03-03 15:04:34 +0800

    up

--- src/SkillHub/index.ts
+++ src/SkillHub/index.ts
@@ -1,4 +1,5 @@
-export type { HubSkillResult, HubInstallResult, InstallSkillOptions, ISkillHubService, SkillHubProvider } from './types';
+export type { HubSkillResult, HubInstallResult, InstallSkillOptions, ISkillHubService } from './types';
+export { SkillHubProvider } from './types';
 export { ClawhubSkillHubService } from './ClawhubSkillHubService';
 export { SkillsShSkillHubService } from './SkillsShSkillHubService';
 export { SkillsmpSkillHubService } from './SkillsmpSkillHubService';
