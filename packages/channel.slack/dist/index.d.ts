b33adee1a5ca6f9ed12e78cd2792c0c7fc7e52d9
linyuan.yang <linyuan.yang@centurygame.com>
2026-03-18 16:11:34 +0800

    支持 channel.slack

--- packages/channel.slack/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { SlackService, SlackServiceOptions } from './SlackService';
-export { SlackChatProvider } from './SlackChatProvider';
-export { SlackUserServiceBase, SlackMessageArgs, SlackActionArgs, ToolCallStatus, } from './SlackUserServiceBase';
-//# sourceMappingURL=index.d.ts.map
