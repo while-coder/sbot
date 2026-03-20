d4e2c883b15423f6534794417c44a6d3af3b4ef8
linyuan.yang <linyuan.yang@centurygame.com>
2026-03-20 17:16:26 +0800

    feat: export SessionManager from channel.base

--- packages/channel.base/src/index.ts
+++ packages/channel.base/src/index.ts
@@ -1,2 +1,3 @@
 export { ChannelUserServiceBase, ToolCallStatus } from './ChannelUserServiceBase';
 export { ProviderMessageType, ProviderTextMessage, ProviderToolMessage, ProviderMessage, parseMessages2Text } from './ProviderMessage';
+export { sessionManager, SessionStatus, SessionInfo, AskInfo, PendingToolInfo } from './SessionManager';
